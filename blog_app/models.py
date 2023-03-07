960bd28f5750e86b2e6b712870ec768536a9fb9a
sakhawat46 <hsakhawat135@gmail.com>
2023-03-07 10:58:07 +0600

    blog update

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 class Blog(models.Model):
     blog_title = models.CharField(max_length=255, verbose_name='Put a Title')
-    blog_content = models.TextField(verbose_name='Blog Content Write Here')
+    short_description = models.TextField(max_length=500)
+    blog_content = RichTextField()
     blog_image = models.ImageField(upload_to='blog_images', verbose_name='Image', blank=False, null=False)
     publish_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
